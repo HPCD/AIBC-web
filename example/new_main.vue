2babc26502c5c395a4556e11e691616435b70da0
abner2015 <abner2015@qq.com>
2020-08-31 15:14:44 +0800

    添加标签和坐标的ajax

--- example/new_main.vue
+++ example/new_main.vue
@@ -82,6 +82,11 @@ export default {
     if (localStorage.getItem("imgInfos")) {
       // this.imgInfos = JSON.parse(localStorage.getItem("imgInfos"));
     }
+    // alert("cre");
+    let _this = this;
+    
+    this.initLabelData();
+    console.log("结束之后为 ", _this.labels);
   },
   mounted() {
     //  设置图片缩略图
@@ -115,6 +120,33 @@ export default {
     this.onloadMenu();
   },
   methods: {
+    // 初始化请求数据，如标签，坐标信息
+    initLabelData() {
+      var hurl = "http://127.0.0.1:9999/src/labels";
+      // alert("d");
+      let _this = this;
+      //后台请求数据
+      $.ajax({
+        url: hurl,
+        type: "post",
+        data: { user_name: "123", device_id: "123456" },
+        dataType: "json",
+        success: function (data, dataTextStatus, jqxhr) {
+          console.log("初始化返回数据 ： ", data);
+          _this.labels = data.data;
+          // alert(this.labels);
+          // return data.data
+        },
+        error: function (jqxhr, textStatus, error) {
+          console.log("__失败__");
+          console.log(error);
+          console.log(textStatus);
+          console.log(jqxhr);
+          console.log("******");
+        },
+      });
+    },
+
     //添加右击事件并初始化
     onloadMenu() {
       //需要通过_this 方能获取到contextMenuItems,否则为未识别到
@@ -135,6 +167,7 @@ export default {
         },
       });
     },
+    //初始化画板
     initCanvas() {
       let _this = this;
       //  设置canvas
@@ -221,11 +254,49 @@ export default {
         // singlerRect.lastPoint = this.lastPoint;
       });
     },
+    //提交上一张图片信息进行保存
+    saveLastImageInfo(lastImageInfo){
+      console.log("saveLast ",lastImageInfo)
+      var imageInfo =  JSON.stringify(lastImageInfo);
+      var n = {}
+      n.url= lastImageInfo.url
+      n.rects_list = lastImageInfo.rects_list
+      var jsonObj = JSON.stringify(n);
+      // alert(jsonObj)
+      console.log("json字符串 ",n,jsonObj)
+      var save_imgInfo_url = "http://127.0.0.1:9999/src/img_info";
+      //异步请求
+      $.ajax({
+        url: save_imgInfo_url,
+        type: "put",
+        data: { user_name: "123", device_id: "123456",single_img_info:jsonObj },
+        dataType: "json",
+        success: function (data, dataTextStatus, jqxhr) {
+          console.log("返回数据 ： ", data);
+          // _this.labels = data.data;
+          alert(data.data);
+          // return data.data
+        },
+        error: function (jqxhr, textStatus, error) {
+          console.log("__失败__");
+          console.log(error);
+          console.log(textStatus);
+          console.log(jqxhr);
+          console.log("******");
+        },
+      });
+
+    },
+
+    // 当点击下一张图片时设置背景图并画出矩形框
     setImgCanvas(url) {
       // this.drawRect(150,150,30,70);
       //console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
       var _this = this;
       console.log("查看imgInfo有多少 ： ", _this.imgInfos);
+      console.log("simg ",_this.singleImg);
+      //提交上一张图片信息保存
+      this.saveLastImageInfo(_this.singleImg),
       // 首先清空之前画布
       _this.canvas.clear();
       console.log("上一张 ", _this.singleImg.url);
@@ -242,7 +313,7 @@ export default {
       //从imgInfo中加载图片信息
       //判断imgInfos中是否已经存在那张图片,如果存在取index,加载坐标信息，如果不在则把url 信息加载添加到imgInfos中
       if (_this.imgInfos.length == 0) {
-        alert("首次加载");
+        // alert("首次加载");
         //第一次加载
         console.log("首次加载的url", _this.singleImg.url);
         _this.imgInfos.push(_this.singleImg);
@@ -295,34 +366,30 @@ export default {
               var strs = _this.rects_list[i].split("&");
               var cords = strs[0].split("-");
               var rectWH = strs[1].split("-");
-              console.log("strs.length",strs.length);
+              console.log("strs.length", strs.length);
               // 添加标签,还没把没有标签的情形房间去考虑
-              if(strs.length==3){
-                
+              if (strs.length == 3) {
                 var label_name = strs[2];
-                 _this.drawRectAndText(label_name,
-                //调了位置，不然矩形有问题
-                parseFloat(cords[1]),
-                parseFloat(cords[0]),
-                parseFloat(rectWH[0]),
-                parseFloat(rectWH[1])
-              );
-
-              }else{
+                _this.drawRectAndText(
+                  label_name,
+                  //调了位置，不然矩形有问题
+                  parseFloat(cords[1]),
+                  parseFloat(cords[0]),
+                  parseFloat(rectWH[0]),
+                  parseFloat(rectWH[1])
+                );
+              } else {
                 _this.drawRect(
-                parseFloat(cords[0]),
-                parseFloat(cords[1]),
-                parseFloat(rectWH[0]),
-                parseFloat(rectWH[1])
-              );
-
+                  parseFloat(cords[0]),
+                  parseFloat(cords[1]),
+                  parseFloat(rectWH[0]),
+                  parseFloat(rectWH[1])
+                );
               }
-              
+
               console.log("开始绘制了，准备");
               // console.log("parseFloat(cords[0])",parseFloat(cords[0]))
               //  console.log("parseFloat(cords[1])",parseFloat(cords[1]))
-              
-              
 
               // this.drawRect(200,280,30,70);
             }
@@ -359,7 +426,7 @@ export default {
     },
     // 画矩形
     drawRect(t, l, w, h) {
-      console.log("@@@@@@@@@@@@@@@@@@@", t, l, w, h);
+      // console.log("@@@@@@@@@@@@@@@@@@@", t, l, w, h);
       var rect = new fabric.Rect({
         top: l, //距离画布上边的距离
         left: t, //距离画布左侧的距离，单位是像素
@@ -403,19 +470,17 @@ export default {
     },
 
     // 动态加载菜单
-    loadMean(object){
-
+    loadMean(object) {
       var _this = this;
       //循环遍历标签数组加载标签项到右键菜单
-      for(var i=0;i<_this.labels.length;i++){
+      for (var i = 0; i < _this.labels.length; i++) {
         var obj = {};
         obj["name"] = _this.labels[i];
-        obj["icon"] =  "label";
-        obj["data"] =  object;
+        obj["icon"] = "label";
+        obj["data"] = object;
         // 加入到菜单
         _this.contextMenuItems[`label${i}`] = obj;
       }
-
     },
 
     //显示菜单内容
@@ -426,7 +491,7 @@ export default {
         delete: { name: "删除", icon: "delete", data: object },
         add: { name: "新增标签", icon: "add", data: object },
       };
-       
+
       //动态加载菜单
       this.loadMean(object);
       //右键菜单显示位置
@@ -446,7 +511,7 @@ export default {
         //let _this = this;
         //得到对应的object并删除
         var object = this.contextMenuItems[key].data;
-        
+
         var temp_rect =
           object.left +
           "-" +
@@ -464,16 +529,17 @@ export default {
         _this.rects_list.splice(index, 1);
         //console.log("delete list",_this.rects_list)
         this.canvas.remove(object); //删除矩形框
-      } else if (key == "label0") {
-        console.log("输出的标签为: ",key,this.contextMenuItems);
+      } else {
+        console.log("输出的标签为: ", key, this.contextMenuItems);
         var new_key = key;
-        console.log("根据key获得",this.contextMenuItems[new_key]);
-        var label_name = this.contextMenuItems[key].name
-        console.log('根据key 获得的name ',label_name);
+        console.log("根据key获得", this.contextMenuItems[new_key]);
+        var label_name = this.contextMenuItems[key].name;
+        console.log("根据key 获得的name ", label_name);
         // 把标签和坐标保存
         var object = this.contextMenuItems[key].data;
         //没有标签的的矩形框
-        var temp_rect =  object.left +
+        var temp_rect =
+          object.left +
           "-" +
           object.top +
           "&" +
@@ -482,94 +548,65 @@ export default {
           object.height;
 
         //增加了标签的矩形框
-        var temp_rect_label =  object.left +
+        var temp_rect_label =
+          object.left +
           "-" +
           object.top +
           "&" +
           object.width +
           "-" +
           object.height +
-          "&"+
+          "&" +
           label_name;
         //获取到矩形所在的索引
         var index = _this.rects_list.indexOf(temp_rect);
-        console.log("删除之前的矩形信息",_this.rects_list);
+        console.log("删除之前的矩形信息", _this.rects_list);
         //删除矩形信息
         _this.rects_list.splice(index, 1);
         //重新添加矩形信息
         _this.rects_list.push(temp_rect_label);
-        console.log("查看更新标签后的矩形信息",_this.rects_list);
-        //把矩形框和文本框画出来
-        //1 文本框
-        // var text = new fabric.Text(label_name, {
-        //   originX: 'center',
-        //   originY: 'center',
-        //   left: object.left, 
-        //   top: object.top,
-        //   fill: 'red',
-        //   });
-        // 2 矩形框
-        // var rect = new fabric.Rect({
-        // top: object.top, //距离画布上边的距离
-        // left:object.left, //距离画布左侧的距离，单位是像素
-        // width: object.width, //矩形的宽度
-        // height: object.height, //矩形的高度
-        // fill: "#00000000",
-        // evented: false, //rect 无法被选中
-        // stroke: "red", // 边框原色
-        // strokeWidth: 3, // 边框大小
-        // });
+        console.log("查看更新标签后的矩形信息", _this.rects_list);
 
-        // var group = new fabric.Group(
-        //   [rect, text]);
-
-        // //删除矩形框
-        // _this.canvas.remove(_this.canvas.getActiveObject());
-        // //3 添加矩形和文本信息
-        // _this.canvas.add(group);
         var top = object.top;
         var left = object.left;
         var w = object.width;
-        var h = object.height
-        this.drawRectAndText(label_name,top,left,w,h);
+        var h = object.height;
+        this.drawRectAndText(label_name, top, left, w, h);
         console.log("label", key);
       }
     },
 
-    drawRectAndText(label_name,l,t,w,h){
+    drawRectAndText(label_name, l, t, w, h) {
       let _this = this;
-        //1 文本框
-        var text = new fabric.Text(label_name, {
-          originX: 'center',
-          originY: 'center',
-          left: t-4, 
-          top: l-4,
-          fill: 'yellow',
-          });
+      //1 文本框
+      var text = new fabric.Text(label_name, {
+        originX: "center",
+        originY: "center",
+        left: t - 4,
+        top: l - 4,
+        fill: "yellow",
+      });
 
-          // 2 矩形框
-        var rect = new fabric.Rect({
-          top: l, //距离画布上边的距离
-          left:t, //距离画布左侧的距离，单位是像素
-          width: w, //矩形的宽度
-          height: h, //矩形的高度
-          fill: "#00000000",
-          evented: false, //rect 无法被选中
-          stroke: "red", // 边框原色
-          strokeWidth: 3, // 边框大小
-        });
+      // 2 矩形框
+      var rect = new fabric.Rect({
+        top: l, //距离画布上边的距离
+        left: t, //距离画布左侧的距离，单位是像素
+        width: w, //矩形的宽度
+        height: h, //矩形的高度
+        fill: "#00000000",
+        evented: false, //rect 无法被选中
+        stroke: "red", // 边框原色
+        strokeWidth: 3, // 边框大小
+      });
 
-        //组合矩形和文本
-        var group = new fabric.Group(
-          [rect, text]);
+      //组合矩形和文本
+      var group = new fabric.Group([rect, text]);
 
-        //删除矩形框
-        _this.canvas.remove(_this.canvas.getActiveObject());
-        //3 添加矩形和文本信息
-        _this.canvas.add(group);
-
+      //删除矩形框
+      _this.canvas.remove(_this.canvas.getActiveObject());
+      //3 添加矩形和文本信息
+      _this.canvas.add(group);
     },
-
 
     getImginfo() {
       //发送ajax请求，将数据请求到放到imgInfos中,在munted中进行调用################################
@@ -585,6 +622,8 @@ export default {
       })
         .then(({ value }) => {
           _this.labels.push(value);
+          //保存数据到后台
+          _this.saveLabels(value);
           this.$message({
             type: "success",
             message: "你的标签是: " + value,
@@ -596,6 +635,32 @@ export default {
             message: "取消输入",
           });
         });
+    },
+    //保存坐标
+    saveLabels(value) {
+      var save_label_url = "http://127.0.0.1:9999/src/labels";
+      // alert("d");
+      let _this = this;
+      //后台请求数据
+      $.ajax({
+        url: save_label_url,
+        type: "put",
+        data: { user_name: "123", device_id: "123456",label:value },
+        dataType: "json",
+        success: function (data, dataTextStatus, jqxhr) {
+          console.log("初始化返回数据 ： ", data);
+          // _this.labels = data.data;
+          alert(data.data);
+          // return data.data
+        },
+        error: function (jqxhr, textStatus, error) {
+          console.log("__失败__");
+          console.log(error);
+          console.log(textStatus);
+          console.log(jqxhr);
+          console.log("******");
+        },
+      });
     },
   },
 };
